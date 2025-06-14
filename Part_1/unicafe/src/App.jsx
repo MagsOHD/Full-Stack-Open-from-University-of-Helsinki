5afa82c7d3d498120edd1b35cdb0c2d91681287b
Mags <lucas.bracq@gmail.com>
2025-06-14 15:36:53 +0200

    unicafe step 2 + corrected NaN

--- Part_1/unicafe/src/App.jsx
+++ Part_1/unicafe/src/App.jsx
@@ -5,22 +5,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
   const texts = {
     headerText: "Give feedback",
     tilteText: "Statistics",
     goodClick: "Good",
     badClick: "Bad",
-    neutralClick: "Neutral"
+    neutralClick: "Neutral",
+    average: "Average",
+    positive: "Positive",
+    all: "All"
   }
   const handleBadClick = () => {
+    setAll(all + 1)
     setBad(bad + 1)
   }
 
   const handleNeutralClick = () => {
+    setAll(all + 1)
     setNeutral(neutral + 1)
   }
 
   const handleGoodClick = () =>{
+    setAll(all + 1)
     setGood(good + 1)
   }
 
@@ -34,6 +42,9 @@ const App = () => {
       <Statistics text={texts.goodClick} nb={good}/>
       <Statistics text={texts.neutralClick} nb={neutral}/>
       <Statistics text={texts.badClick} nb={bad}/>
+      <Statistics text={texts.all} nb={all}/>
+      <Statistics text={texts.average} nb={all != 0 ? ((good - bad)/all) : 0}/>
+      <Statistics text={texts.positive} nb={ all != 0 ? (good * 100 / all) : 0} pct='%'/>
     </div>
   )
 }
@@ -41,10 +52,10 @@ const App = () => {
 const Header = ({text}) => <h1>{text}</h1>
 const Titles = ({text}) => <h2>{text}</h2>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-const Statistics = ({text, nb}) => {
+const Statistics = ({text, nb, pct=''}) => {
   return (
   <>
-    <p>{text}: {nb}</p>
+    <p>{text}: {nb}{pct}</p>
   </>)
 }
 
